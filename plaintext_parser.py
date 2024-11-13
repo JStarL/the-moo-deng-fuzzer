7aa2442f692a5f4d6e7159149abc787560e516a7
Jibitesh Saha <jibiteshsaha@gmail.com>
2024-11-13 15:38:54 +1100

    1) processing in bytes, not str 2) write_binary_input 3) txt_fuzz_processor

--- plaintext_parser.py
+++ plaintext_parser.py
@@ -1,7 +1,8 @@
 from utils import FieldType, determine_input_type, field_fuzzer
 from typing import List, Iterator
+import copy
 
-def read_txt_file(filename: str) -> bytes:
+def read_txt_file(filename: str) -> List[bytes]:
     with open(filename, 'rb') as f:
         lines = f.read()
         return lines.split(b'\n')
@@ -14,5 +15,35 @@ def process_txt(lines: List[bytes]) -> List[FieldType]:
 
     return line_type
 
-def txt_fuzz_processor(lines: List[bytes], line_type: List[FieldType]) -> Iterator[bytes]:
-    pass
+def write_binary_input(mod_input):
+    for i, elem in enumerate(mod_input):
+        if not isinstance(elem, bytes):
+            mod_input[i] = str(elem).encode()
+    
+    return b"\n".join(mod_input)
+
+def txt_fuzz_processor(lines: List[bytes], line_type: List[FieldType]) -> Iterator[List[bytes]]:
+    
+    fuzzers = [field_fuzzer(line_type[i], f'Line {i}', lines[i]) for i in range(len(lines))]
+    i = 0
+    complete = [False] * len(lines)
+    complete_count = 0
+
+    while complete_count < len(lines):
+
+        if i >= len(fuzzers):
+            i = 0
+        
+        if complete[i]:
+            i += 1
+            continue
+        
+        lines_mod = copy.deepcopy(lines)
+        try:
+            lines_mod[i] = next(fuzzers[i])
+            yield lines_mod
+        except StopIteration:
+            complete[i] = True
+            complete_count += 1
+
+        i += 1

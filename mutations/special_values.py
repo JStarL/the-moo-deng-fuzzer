90cb55db936e9461bd4bfe40e357322663460339
Jibitesh Saha <jibiteshsaha@gmail.com>
2024-11-13 17:15:14 +1100

    added more special characters

--- mutations/special_values.py
+++ mutations/special_values.py
@@ -39,7 +39,7 @@ SPECIAL_INTS: list[int] = list({
 SPECIAL_CHAR_INTS = [0x00, 0xFF, 0x7F, 0x80]
 
 # Define boundary values in string form for testing in text-based fields
-BOUNDARY_CHAR_STRINGS = ["\x00", "\xFF", "\x7F", "\x80"]
+BOUNDARY_CHAR_STRINGS = ["\x00", "\xFF", "\x7F", "\x80", "\x01", "\x02", "\x03", "\x04", "\x05", "\x06", "\x07", "\x08", "\x09", "\x0a", "\x0b", "\x0c", "\x0d", "\x0e", "\x0f", "\x10", "\x11", "\x12", "\x13", "\x14", "\x15", "\x16", "\x17", "\x18", "\x19", "\x1a", "\x1b", "\x1c", "\x1d", "\x1e", "\x1f"]
 
 
 

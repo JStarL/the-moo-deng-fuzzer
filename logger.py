13d7c252129e7325615e1462800a936da1d4fcc0
Halliya UM <halliya070505@gmail.com>
2024-11-14 16:52:04 +1100

    Add details in logger.py

--- logger.py
+++ logger.py
@@ -1,17 +1,29 @@
 import logging
 
-def logger_setup():
-    # create logger
+def logger_setup(log_type):
     logger = logging.getLogger()
-    # log_file = logging.FileHandler('fuzzer.log')
-    logger.setLevel(logging.DEBUG)
+    log_file = logging.FileHandler('fuzzer.log')
+    # logger.setLevel(logging.INFO)
+    
+    if log_type == "-d":
+        logging.basicConfig(format='(%(asctime)s) %(levelname)s:%(pathname)s-%(lineno)d:%(message)s ',
+                            datefmt = '%d-%m-%y %I:%M:%S $p',
+                            level=logging.DEBUG)
+                            # have to add more formats thread/process things 
+    else:
+        logging.basicConfig(format='(%(asctime)s) %(levelname)s:%(pathname)s-%(lineno)d:%(message)s ',
+                            datefmt = '%d-%m-%y %I:%M:%S $p',
+                            level=logging.INFO)
+
     # format of the log - timestamp, vulns type + file extension type, file path, error line
     log_format = logging.Formatter(
         %(%d %m, %Y %H:%M:%S)s - %(vultype)s %(\n)s %(fileextension)s %(filepath)s
     )
-    # log_file.setFormatter(log_format)
+    log_file.setFormatter(log_format)
 
     return logger
+
+def 
 
 fuzzer_logger = logger_setup()
 # use : call "from logger import fuzzer_logger"

e6b6be070bac90a0dad1c9ba7f0ba86a8ae39a35
Jibitesh Saha <jibiteshsaha@gmail.com>
2024-11-14 18:29:14 +1100

    corrected and completed logger

--- logger.py
+++ logger.py
@@ -1,30 +1,34 @@
 import logging
+import sys
 
-def logger_setup(log_type):
+def logger_setup(log_type = None):
+    if log_type == "DEBUG":
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    logging.basicConfig(format='(%(asctime)s) %(levelname)s:%(pathname)s-%(lineno)d:%(message)s ',
+                        datefmt='%d-%m-%y %I:%M:%S %p',
+                        level=level)
+    
     logger = logging.getLogger()
     log_file = logging.FileHandler('fuzzer.log')
+    logger.addHandler(log_file)
+    
     # logger.setLevel(logging.INFO)
     
-    if log_type == "-d":
-        logging.basicConfig(format='(%(asctime)s) %(levelname)s:%(pathname)s-%(lineno)d:%(message)s ',
-                            datefmt = '%d-%m-%y %I:%M:%S $p',
-                            level=logging.DEBUG)
-                            # have to add more formats thread/process things 
-    else:
-        logging.basicConfig(format='(%(asctime)s) %(levelname)s:%(pathname)s-%(lineno)d:%(message)s ',
-                            datefmt = '%d-%m-%y %I:%M:%S $p',
-                            level=logging.INFO)
-
-    # format of the log - timestamp, vulns type + file extension type, file path, error line
-    '''
-    log_format = logging.Formatter(
-        %(%d %m, %Y %H:%M:%S)s - %(vultype)s %(\n)s %(fileextension)s %(filepath)s
-    )
-    log_file.setFormatter(log_format)
-    '''
+    if level == logging.DEBUG:
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(logging.Formatter('(%(asctime)s) %(levelname)s: %(message)s'))
+        logger.addHandler(console_handler)
+    
+    
     return logger
 
-def 
+if "-d" in sys.argv or "--debug" in sys.argv or 'DEBUG' in sys.argv:
+    log_type = 'DEBUG'
+else:
+    log_type = None
 
-fuzzer_logger = logger_setup()
+fuzzer_logger = logger_setup(log_type)
 # use : call "from logger import fuzzer_logger"

95952aa24d743cdbc2ec09c8a550ef37b5a3c545
Dysthesis <antheoraviel@protonmail.com>
2024-11-14 22:33:50 +1100

    FIxes

--- coverage/test.py
+++ coverage/test.py
@@ -41,10 +41,10 @@ cfg = ControlFlowGraph(
     )
 
 for vertex in cfg.graph.get_vertices():
-    print(f"Vertex {vertex} has value {hex( cfg.offset_map[vertex] )} and points to {cfg.graph.get_all_neighbors(vertex)}")
+    print(f"Vertex {vertex} has value {hex( cfg.addr_map[vertex] )} and points to {cfg.graph.get_all_neighbors(vertex)}")
 
 print(f"There are { len(blocks) } blocks")
 
 breakpoints = binary.get_blocks(addr)
-breakpoints = map(lambda x: x['offset'], breakpoints)
+breakpoints = map(lambda x: x['addr'], breakpoints)
 binary.run_with_log(breakpoints)

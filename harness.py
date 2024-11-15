541d70bee2339f1d9845e70fb4c5cb33579e3f89
Jibitesh Saha <jibiteshsaha@gmail.com>
2024-11-15 15:37:32 +1100

    added

--- harness.py
+++ harness.py
@@ -16,36 +16,36 @@ from typing import List
 from logger import fuzzer_logger
 
 programs = [
-    # './binaries/json1',
-    # './binaries/json2',
-    # './binaries/my_json'
-    # './binaries/csv1',
-    # './binaries/csv2',
-    # './binaries/my_csv',
-    # './binaries/jpg1',
-    # './binaries/my_jpeg'
-    # './binaries/plaintext1',
-    # './binaries/plaintext2',
+    './binaries/json1',
+    './binaries/json2',
+    './binaries/my_json'
+    './binaries/csv1',
+    './binaries/csv2',
+    './binaries/my_csv',
+    './binaries/jpg1',
+    './binaries/my_jpeg'
+    './binaries/plaintext1',
+    './binaries/plaintext2',
     './binaries/plaintext3',
-    # './binaries/xml1',
-    # './binaries/xml2',
-    # './binaries/xml3',
+    './binaries/xml1',
+    './binaries/xml2',
+    './binaries/xml3',
 ]
 inputs = [
-    # './example_inputs/json1.txt',
-    # './example_inputs/json2.txt',
-    # './example_inputs/my_json.txt'
-    # './example_inputs/csv1.txt',
-    # './example_inputs/csv2.txt'
-    # './example_inputs/my_csv.txt'
-    # './example_inputs/jpg1.txt',
-    # './example_inputs/my_jpeg.txt',
-    # './example_inputs/plaintext1.txt',
-    # './example_inputs/plaintext2.txt',
+    './example_inputs/json1.txt',
+    './example_inputs/json2.txt',
+    './example_inputs/my_json.txt'
+    './example_inputs/csv1.txt',
+    './example_inputs/csv2.txt'
+    './example_inputs/my_csv.txt'
+    './example_inputs/jpg1.txt',
+    './example_inputs/my_jpeg.txt',
+    './example_inputs/plaintext1.txt',
+    './example_inputs/plaintext2.txt',
     './example_inputs/plaintext3.txt',
-    # './example_inputs/xml1.txt',
-    # './example_inputs/xml2.txt',
-    # './example_inputs/xml3.txt',
+    './example_inputs/xml1.txt',
+    './example_inputs/xml2.txt',
+    './example_inputs/xml3.txt',
 ]
 
 class FileType(Enum):
@@ -95,7 +95,7 @@ def run_program(prog_path: str, input: str | bytes, mode: str = 'TEXT', timeout=
         statistics["fuzzer_attempt"] += 1
         statistics["fuzzer_success"] += 1
         statistics["fuzzer_success_rate"] = statistics["fuzzer_success"] / statistics["fuzzer_attempt"] * 100
-        fuzzer_logger.info('Fuzzer success rate(%d out of %d attempt): %d \%',fuzzer_success, fuzzer_attempt, fuzzer_success_rate )
+        fuzzer_logger.info('Fuzzer success rate(%d out of %d attempt): %d \%',statistics["fuzzer_success"], statistics["fuzzer_attempt"], statistics["fuzzer_success_rate"] )
         return True
 
     statistics["fuzzer_attempt"] += 1

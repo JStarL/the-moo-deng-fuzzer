9c3cdd5317272e7176c4a169d16e05dd20ce1c92
Jibitesh Saha <jibiteshsaha@gmail.com>
2024-11-15 15:47:26 +1100

    fixed missing commas

--- harness.py
+++ harness.py
@@ -18,12 +18,12 @@ from logger import fuzzer_logger
 programs = [
     './binaries/json1',
     './binaries/json2',
-    './binaries/my_json'
+    './binaries/my_json',
     './binaries/csv1',
     './binaries/csv2',
     './binaries/my_csv',
     './binaries/jpg1',
-    './binaries/my_jpeg'
+    './binaries/my_jpeg',
     './binaries/plaintext1',
     './binaries/plaintext2',
     './binaries/plaintext3',
@@ -34,10 +34,10 @@ programs = [
 inputs = [
     './example_inputs/json1.txt',
     './example_inputs/json2.txt',
-    './example_inputs/my_json.txt'
+    './example_inputs/my_json.txt',
     './example_inputs/csv1.txt',
-    './example_inputs/csv2.txt'
-    './example_inputs/my_csv.txt'
+    './example_inputs/csv2.txt',
+    './example_inputs/my_csv.txt',
     './example_inputs/jpg1.txt',
     './example_inputs/my_jpeg.txt',
     './example_inputs/plaintext1.txt',

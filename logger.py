45d84ed626622b8439e35365cef9e90c32dfe94b
Halliya UM <halliya070505@gmail.com>
2024-11-15 17:09:41 +1100

    Fix duplicated log format

--- logger.py
+++ logger.py
@@ -15,10 +15,6 @@ def logger_setup(log_type = None):
                         level=level)
 
     logger = logging.getLogger()
-    
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(logging.Formatter('(%(asctime)s) %(levelname)s: %(message)s'))
-    logger.addHandler(console_handler)
 
     return logger
 

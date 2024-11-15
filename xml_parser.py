f5f2bc20fdd4e991deb43ff6947caea024e65548
Jibitesh Saha <jibiteshsaha@gmail.com>
2024-11-15 11:48:34 +1100

    implement round robin for xml fuzz processor and enable nested mutations

--- xml_parser.py
+++ xml_parser.py
@@ -4,7 +4,7 @@ import copy
 from utils import determine_input_type
 from mutations.xml_mutation import xml_text_mutation, xml_attr_mutation, xml_tag_mutation, xml_nested_mutation
 import sys
-
+from logger import fuzzer_logger
 
 def read_xml_file(filepath: str):
     try:
@@ -33,7 +33,7 @@ def process_xml(tree: xml.ElementTree):
 def xml_fuzz_processor(tree: xml.ElementTree, file_type_tree: xml.ElementTree) -> Iterator[xml.ElementTree]:
     # Define mutation functions and their descriptions
     mutation_functions = [
-        # xml_nested_mutation,
+        xml_nested_mutation,
         xml_text_mutation,
         xml_attr_mutation,
         xml_tag_mutation,
@@ -42,12 +42,26 @@ def xml_fuzz_processor(tree: xml.ElementTree, file_type_tree: xml.ElementTree) -
 
     mutation_generators = [func(xml.tostring(tree.getroot())) for func in mutation_functions]
 
-    while mutation_generators:
-        for i, generator in enumerate(mutation_generators):
-            try:
-                mutated_xml_bytes = next(generator)
-                yield mutated_xml_bytes
-            except StopIteration:
-                print('generator = ', generator)
-                mutation_generators.pop(i)
+    i = 0
+    while len(mutation_generators) > 0:
+        if i >= len(mutation_generators):
+            i = 0
+
+        try:
+            mutated_xml_bytes = next(mutation_generators[i])
+            yield mutated_xml_bytes
+        except StopIteration:
+            fuzzer_logger.debug(f'finished {mutation_generators[i]} mutations')
+            mutation_generators.pop(i)
+            continue
+
+        i += 1
+    # while mutation_generators:
+    #     for i, generator in enumerate(mutation_generators):
+    #         try:
+    #             mutated_xml_bytes = next(generator)
+    #             yield mutated_xml_bytes
+    #         except StopIteration:
+    #             print('generator = ', generator)
+    #             mutation_generators.pop(i)
 
